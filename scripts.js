7c2d6f31b1773b621dd1146483a44a556dad945b
Melqui Moraiss <melquimoraiss@gmail.com>
2025-04-07 00:47:36 -0300

    add: animation in color button and animate shoe image

--- scripts.js
+++ scripts.js
@@ -3,7 +3,14 @@ let tenis = document.querySelector(".tenis");
 let botao = document.querySelector(".buttoncart");
 
 function mudarVisual(cor, imagem) {
+  tenis.classList.add("troca-efeito");
+
   body.style.background = cor;
-  tenis.src = imagem;
+
   botao.style.background = cor;
+
+  setTimeout(() => {
+    tenis.src = imagem;
+    tenis.classList.remove("troca-efeito");
+  }, 550);
 }
